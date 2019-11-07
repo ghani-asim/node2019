6cd7b1b1b80c33d2f3ba03265e14ce00fdeb6ff5
Asim Ghani <asim.ghani@metropolia.fi>
2019-11-07 13:23:06 +0200

    express app, get / and get /demo

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('My App...');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from my Node server');
+});
+
+app.get('/demo', (req, res) => {
+    res.send('demo');
+});
+
+app.listen(3000, () => {
+    console.log('server app start?');
+});
