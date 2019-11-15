b0ac97e0c3c99cfe83a9b141a2ba62e819a4ff38
Asim Ghani <asim.ghani@metropolia.fi>
2019-11-15 14:02:41 +0200

    database connection and SELECT * under /animal

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 'use strict';
 
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
+const mysql = require('mysql2');
 const app = express();
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+app.use(express.static('public'));
+
+app.get('/animal', (req, res) => {
+    // simple query
+    connection.query(
+        'SELECT * FROM animal',
+        (err, results, fields) => {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+            res.json(results);
+        }
+    );
+});
 
 app.get('/', (req, res) => {
     res.send('Hello from my Node server');
