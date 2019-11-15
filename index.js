69cdfab8bcc20021e2b90050dd08203634044bc5
Asim Ghani <asim.ghani@metropolia.fi>
2019-11-15 14:31:54 +0200

    database connection in seperated module exports

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 'use strict';
 
-require('dotenv').config();
-
 const express = require('express');
-const mysql = require('mysql2');
-const app = express();
+const connection = require('./model/db.js');
 
-// create the connection to database
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
+const app = express();
 
 app.use(express.static('public'));
 
-app.get('/animal', (req, res) => {
+app.get('/animal', async (req, res) => {
     // simple query
-    connection.query(
-        'SELECT * FROM animal',
-        (err, results, fields) => {
-            console.log(results); // results contains rows returned by server
-            console.log(fields); // fields contains extra meta data about results, if available
-            res.json(results);
-        }
-    );
+    try {
+        const [results, fields] = await connection.query(
+            'SELECT * FROM animal');
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        res.json(results);
+    } catch (e) {
+        console.log(e);
+        res.send('db error :(');
+    }
 });
 
 app.get('/', (req, res) => {
