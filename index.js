dd98418b3947f17095b9ba461e2fa779b2cb8576
ghani-asim <asim.ghani@metropolia.fi>
2019-11-21 14:46:08 +0200

    https

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 
 const express = require('express');
 const animal = require('./model/animal');
-
 const app = express();
+
+if(process.env.SERVER === 'dev_localhost') {
+    require('./secure/localhost')(app);
+} else {
+    require('./secure/server')(app);
+    app.listen(3000, () => {
+        console.log('server app start?');
+    });
+}
+
 const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
@@ -38,13 +47,13 @@ app.post('/animal', bodyParser.urlencoded({extended: true}), async (req, res) =>
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello from my Node server');
+    if(req.secure) {
+        res.send('Hello secure');
+    } else {
+        res.send('Hello from my Node server unsecure');
+    }    
 });
 
 app.get('/demo', (req, res) => {
     res.send('demo');
-});
-
-app.listen(3000, () => {
-    console.log('server app start?');
 });
